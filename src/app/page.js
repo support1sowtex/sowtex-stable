117e841ff61788c82102ea649e2a33760df56cde
Your Name <negikaithal@gmail.com>
2025-06-02 11:04:14 +0530

    divyuum

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,241 @@ function HomePage() {
   return (
     <>
       <Layout />
-    hi
+      {/* <Breadcrumbs /> */}
+      {/* <Carousel  /> */}
+      <section className="source-global-item section-padding" style={{display:'flex',textAlign:'center',justifyContent:'center' }}>
+        <div className="w-1073 d-flex source-global-wrapper center-text">
+          <div className="all-heading text-center">
+            <h1>Source Global Suppliers</h1>
+            
+            <p>The B2B platform to widen your supplier network</p>
+          </div>
+        </div>
+      </section>
+      <div className="container-fluid" style={{display:'flex',textAlign:'center',justifyContent:'space-around',width:'60%' }}>
+          <div className="source-global-box ">
+            <div className="source-global-box-img">
+              <img
+                src="https://sowtex.com/assets/front/img/Source Global Suppliers/Design _ Products.png"
+                alt="Design & Products"
+              />
+            </div>
+            <div className="source-global-box-content">
+              <p>
+                Design & Products<br />
+                <span className="counter" data-target="16874" >
+                  16874+
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="source-global-box">
+            <div className="source-global-box-img">
+              <img
+                src="https://sowtex.com/assets/front/img/Source Global Suppliers/Ready Stocks.png"
+                alt="Ready Stocks"
+              />
+            </div>
+            <div className="source-global-box-content">
+              <p>
+                Ready
+                <br /> Stocks
+                <span className="counter" data-target="817">
+                  817+
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="source-global-box">
+            <div className="source-global-box-img">
+              <img
+                src="https://sowtex.com/assets/front/img/Source Global Suppliers/Reviews _ Ratings.png"
+                alt="Vendor Portfolios"
+              />
+            </div>
+            <div className="source-global-box-content">
+              <p>
+                Vendor
+                <br /> Portfolios
+                <span className="counter" data-target="27555">
+                  27555+
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="source-global-box">
+            <div className="source-global-box-img">
+              <img
+                src="https://sowtex.com/assets/front/img/Source Global Suppliers/Vendor Portfolios.png"
+                alt="Reviews & Ratings"
+              />
+            </div>
+            <div className="source-global-box-content">
+              <p>
+                Reviews &<br /> Ratings
+                <span className="counter" data-target="550">
+                  550+
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* uploading */}
+      <section className="know-more-item section-padding">
+        <div className="container">
+          <div className="row d-flex">
+            <div className="col-md-6">
+              <div className="know-more-content">
+                <h4>SOWTEX as Business Growth Platform</h4>
+                <p>
+                  <strong>Sowtex Network</strong> is a global
+                  <strong>B2B Sourcing Platform</strong> for Fashion and Textile
+                  materials, enabling Buyers and Suppliers to source
+                  <strong>Sustainable Materials</strong>, designs and vendors
+                  across the value chain. We support business deals through
+                  faster matchmaking of <strong>Focused Opportunities</strong>,
+                  further aiding deal closures using a
+                  <strong>Secured Gateway</strong> application.
+                  <br />
+                  As a team, we support both manufacturers as business growth
+                  partners and buyers with faster sourcing solutions,
+                  eliminating supply chain complexities. We are developing
+                  cutting-edge solutions for traditional supply chain problems
+                  through
+                  <strong>
+                    Business Intelligence (BI) and Artificial Intelligence (AI)
+                  </strong>
+                  . Our team is passionate, dynamic, and solution-oriented, with
+                  a mix of young and experienced professionals backed by
+                  industry expertise and driven entrepreneurs. Together, we have
+                  built strong industry collaborations and are on a mission to
+                  transform the conventional sourcing landscape, making it
+                  future-ready.
+                </p>
+                <div className="know-more-content-link text-center">
+                  <a
+                    href="https://sowtex.com/about-us"
+                    className="btn btn-primary btn-img px-3 py-2 mb-3"
+                  >
+                    Know More
+                    <img
+                      src="https://sowtex.com/assets/img/btn-arrow.png"
+                      alt="button"
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 m-0">
+              <div className="know-more-content-icon m-0">
+                <h3>Our Core Values</h3>
+                <div className="flr">
+                  <div className="container flr-sm">
+                    <div className="flx flx-4">
+                      <div className="list-column d-flex p-0">
+                        <img
+                          src="https://sowtex.com/assets/front/img/Speed%20%20Innovation.png"
+                          alt="icon"
+                        />
+                        <p>Speed & Innovation</p>
+                      </div>
+                    </div>
+                    <div className="flx flx-4">
+                      <div className="list-column d-flex p-0">
+                        <img
+                          src="https://sowtex.com/assets/front/img/Customer Centric Solutions_1.png"
+                          alt="icon"
+                        />
+                        <p>Customer Centric Solutions</p>
+                      </div>
+                    </div>
+                    <div className="flx flx-4">
+                      <div className="list-column d-flex p-0">
+                        <img
+                          src="https://sowtex.com/assets/front/img/Focus on Deals Business Growth.png"
+                          alt="icon"
+                        />
+                        <p>Focus on Deals & Business Growth</p>
+                      </div>
+                    </div>
+                    <div className="flx flx-4">
+                      <div className="list-column d-flex p-0">
+                        <img
+                          src="https://sowtex.com/assets/front/img/Empowered Businesses_1.png"
+                          alt="icon"
+                        />
+                        <p>Empowered Businesses</p>
+                      </div>
+                    </div>
+                    <div className="flx flx-4">
+                      <div className="list-column d-flex p-0">
+                        <img
+                          src="https://sowtex.com/assets/front/img/Industry Collaboration_1.png"
+                          alt="icon"
+                        />
+                        <p>Industry Collaboration</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+      </section>
+      <section className="what-we-Offer section-padding">
+            <div className="w-1073 d-flex source-global-wrapper">
+                <div className="all-heading text-center">
+                    <h2>What We Offer?</h2>
+                </div>
+                 <div className="source-global-box">
+                    <div className="source-global-box-img">
+                        <Image src="https://sowtex.com/assets/front/img/Focused Lead Generation.png" alt="icon" width={50} height={50}/>
+                    </div>
+                    <div className="source-global-box-content">
+                        <p>Focused Lead Generation</p>
+                    </div>
+                </div>
+               <div className="source-global-box">
+                    <div className="source-global-box-img">
+                        <Image src="https://sowtex.com/assets/front/img/Vendor Portfolios.png" alt="icon"  width={50} height={50}/>
+                    </div>
+                    <div className="source-global-box-content">
+                        <p>Vendor Portfolios</p>
+                    </div>
+                </div>
+                <div className="source-global-box">
+                    <div className="source-global-box-img">
+                        <Image src="https://sowtex.com/assets/front/img/Customised sourcing solutions.png" alt="icon"  width={50} height={50}/>
+                    </div>
+                    <div className="source-global-box-content">
+                        <p>Customised Sourcing Solutions</p>
+                    </div>
+                </div>
+                <div className="source-global-box">
+                    <div className="source-global-box-img">
+                        <Image src="https://sowtex.com/assets/front/img/Ready Stock.png" alt="icon" width={50} height={50}/>
+                    </div>
+                    <div className="source-global-box-content">
+                        <p>Ready Stock</p>
+                    </div>
+                </div>
+                <div className="source-global-box">
+                    <div className="source-global-box-img">
+                        <Image src="https://sowtex.com/assets/front/img/Secure Deals.png" alt="icon"  width={50} height={50}/>
+                    </div>
+                    <div className="source-global-box-content">
+                        <p>Secure Deals</p>
+                    </div>
+                </div> 
+            </div>
+      </section>
+      <div style={{width:'60%' }} className="container-fluid" >
+      {/* <HomePageSlideshow /> */}
+      </div>
+      {/* <Footer/> */}
+        <br></br>
     </>
   );
 }
--- src/app/products/view-product/[id]/page.js
+++ src/app/products/view-product/[id]/page.js
@@ -45,6 +45,137 @@ export default function ManageProducts() {
   return (
     <>
     <AdminMenu></AdminMenu>
+    <div id="main" style={{ marginLeft: "220px" }}>
+    <div className="container">
+      <section className="card top">
+         <div className="card-body">
+            <div className="row d-flex align-items-center">
+               <div className="col-sm">
+                  <div className="page-title">Product Details</div>
+               </div>
+               <div className="col-sm align-self-end text-end">
+                  <div className="d-none d-sm-block"><a href=""><i className="fa fa-angle-double-left" aria-hidden="true"></i> Back</a> </div>
+                  <div className="breadcrumb-box">
+                     <nav aria-label="breadcrumb">
+                        <ol className="breadcrumb">
+                           <li className="breadcrumb-item"><a href="#"><i className="fa fa-home" aria-hidden="true"></i></a></li>
+                           <li className="breadcrumb-item"><a href="#">Product</a></li>
+                           <li className="breadcrumb-item active" aria-current="page">Product Details</li>
+                        </ol>
+                     </nav>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <section className="content card">
+         <div className="card-body">
+            <div className="row" id="product-detail">
+               <div className="col-sm-4">
+                               
+                  <section id="default" className="padding-top0">
+                     <div className="row">
+                     <Zoom>
+                     <img
+                        alt="That Wanaka Tree, New Zealand by Laura Smetsers"
+                        src="https://sowtex.com/assets/images/Product/S38SD003141283.jpeg"
+                        width="500"
+                     />
+                   </Zoom>
+                     </div>
+                  </section>
+               </div>
+               <div className="col-sm-5">
+  <div className="row mb-3">
+    <div className="col-sm-7">
+      <strong>Article Number</strong> 7021
+    </div>
+    <div className="col-sm-5 text-left">
+      <strong>Visibility:</strong> Public
+    </div>
+  </div>
+
+  <h1 className="p-0">
+    Product Code: <span>S38SD00314</span>
+  </h1>
+
+  <p className="text-muted">
+    <small>Published on: 2025-05-07</small>
+  </p>
+
+  <div className="row">
+    <div className="col-sm-8 m-0">
+      <div className="d-flex">
+        <div style={{ width: "100px", float: "left" }}>
+          <strong>Category</strong>
+        </div>
+        <div>: Denim-Fabrics</div>
+      </div>
+
+      <div className="d-flex">
+        <div style={{ width: "100px" }}>
+          <strong>Sub Category</strong>
+        </div>
+        <div>: Denim with Spandex</div>
+      </div>
+
+      <div className="d-flex">
+        <div style={{ width: "100px" }}>
+          <strong>Color Finish</strong>
+        </div>
+        <div>: Others</div>
+      </div>
+
+      <div className="d-flex">
+        <div style={{ width: "100px" }}>
+          <strong>Size</strong>
+        </div>
+        <div>: 56&nbsp;&quot;</div>
+      </div>
+    </div>
+  </div>
+</div>
+
+               <div className="col-sm-3">
+                  <div className="category-brief bg-light py-3">
+                     <div className="category-image px-2">
+                        <img src="https://sowtex.com/assets/images/logos/173376900087036.jpg" alt="" className="img-fluid w-100 img-thumbnail" />
+                     </div>
+
+                     
+                        <div className="d-flex">
+                           <div style={{width: '120px'}}><strong>Location</strong></div>
+                           <div>: Gujarat, India                           </div>
+                        </div>
+                        <div className="d-flex">
+                           <div style={{width: '120px'}}><strong>Year of Estb.:</strong></div>
+                           <div>: 1970                            </div>
+                        </div>
+                        <div className="d-flex">
+                           <div style={{width: '120px'}}><strong>No. of Employees</strong></div>
+                           <div>: 11-25                           </div>
+                        </div>
+
+                        <div className="d-flex">
+                           <div style={{width: '120px'}}><strong>Membership Plan</strong></div>
+                           <div>: Trial                           </div>
+                        </div>
+                   
+
+                  </div>
+                  <div><a target="__blank" className="mt-2 btn btn-warning" href="https://sowtex.com/future-textile-manufacturing-company/denim-fabrics/denim-with-spandex-/product-detail/S38SD00314">
+                        View Product</a></div>
+               </div>
+            </div>
+            <div className="mt-5 text-center">
+               <a className="btn btn-primary" href="https://sowtex.com/control-panel/edit-product-admin/23200"> <i className="fa fa-pencil" aria-hidden="true"></i> Edit</a>
+            </div>
+         </div>
+      </section>
+   </div>
+
+    </div>
+
    
     
 
